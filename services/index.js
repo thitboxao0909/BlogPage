b0eb588150b266becaa92c6a56feace4f24d6463
TBX <thitboxao0909@gmail.com>
2022-07-29 16:22:09 +1000

    homepage

--- services/index.js
+++ services/index.js
@@ -37,3 +37,59 @@ export const getPosts = async () => {
 
   return results.postsConnection.edges;
 }
+
+export const getRecentPosts = async () => {
+  const query = gql `
+    query getPostDetails(){
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+  const results = await request(graphqlAPI, query);
+
+  return results.posts;
+}
+
+export const getSimilarPosts = async () => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {slug_not: $slug, AND: {categories_some { slug_in: $categories}}}
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+  const results = await request(graphqlAPI, query);
+
+  return results.posts;
+}
+
+export const getCategories = async () => {
+  const query = gql`
+    query GetCategories {
+      categories {
+        name
+        slug
+      }
+    }
+  `
+  const results = await request(graphqlAPI, query);
+
+  return results.categories;
+}
